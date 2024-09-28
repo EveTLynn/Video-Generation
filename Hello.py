fd973ff8b0ff15464f4de1bc8b801766d3d39cde
EveTLynn <linhtong1201@gmail.com>
2024-09-28 20:52:16 +0700

    Add 1st app version

---
+++ Hello.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Chat with Ngrok")
+
+    
+st.write("# Welcome to AI App 👋")
+
+st.markdown(
+    """
+        List Backend was listed here:
+        - [Video Generation](https://colab.research.google.com/drive/12fgJ3r8L73hfjKfNnIV2RAtEJvf9qwZf)
+
+    """
+)
